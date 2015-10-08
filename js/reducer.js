3277783fab63819fa49e2db84bb8181e76d0770c
mmillet <1007650009@qq.com>
2015-10-08 11:23:41 +0800

    reducers

--- js/reducer.js
+++ js/reducer.js
@@ -5,21 +5,26 @@ import {combineReducers} from "./node_modules/redux/dist/redux";
 import {INCREMENT, DECREMENT} from "./actionTypes";
 import {increment, decrement} from "./handle";
 
+let history = (state = [], action = {}) => {
+  state.unshift(action);
+  return state.splice(0, 15);
+};
+
+let items = (state = [], action = {}) => {
+  switch (action.type) {
+    case INCREMENT:
+      increment(state[action.index]);
+      break;
+    case DECREMENT:
+      decrement(state[action.index]);
+      break;
+  }
+  return state;
+};
+
+
 //combine rootReducer
 export default combineReducers({
-  history(state = [], action = {}) {
-    state.unshift(action);
-    return state.splice(0, 15);
-  },
-  items(state = [], action = {}) {
-    switch (action.type) {
-      case INCREMENT:
-        increment(state[action.index]);
-        break;
-      case DECREMENT:
-        decrement(state[action.index]);
-        break;
-    }
-    return state;
-  }
+  history,
+  items
 });
